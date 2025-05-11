e39bb1f667ab6d7853e36bd05f8c7079c6fe89a8
113119134HAUNG <F113119134@nkust.edu.tw>
2025-05-12 02:44:03 +0800

    app.py

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ def main():
                 """)
 
     demo.queue()
-    demo.launch(server_name="127.0.0.1", server_port=7860, show_api=False)
+    demo.launch(server_name="0.0.0.0", server_port=7860, show_api=False)
 
 if __name__ == "__main__":
     main()
